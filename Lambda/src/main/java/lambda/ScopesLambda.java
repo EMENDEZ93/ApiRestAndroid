07e8da736ba0d7f17d636367f5a295202801ffcf
Edwin Mendez <emendez@gravitalsoftware.com>
2018-09-02 20:15:23 -0500

    Scope class

--- Lambda/src/main/java/lambda/ScopesLambda.java
+++ Lambda/src/main/java/lambda/ScopesLambda.java
@@ -2,4 +2,40 @@ package lambda;
 
 public class ScopesLambda {
 
+	final double n3 = 3;
+	
+	public double probarLocalVariable() {
+	
+		Operacion op = new Operacion() {
+
+		@Override
+		public double promedio(double a, double b) {
+				return a + b + n3;
+			}
+		};
+		
+		Operacion operacion = (x, y) -> {
+			return x + y; 
+		};
+		
+		return op.promedio(3, 5);
+	}	
+	
+	public double probarAtributosStaticVariable() {
+		Operacion operacion = (x, y) -> {
+			return (x + y);
+		};
+		return operacion.promedio(3, 3);
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		ScopesLambda scope = new ScopesLambda();
+		
+		System.out.println("**************************");
+		System.out.println(scope.probarLocalVariable());
+	
+	}
+	
 }
